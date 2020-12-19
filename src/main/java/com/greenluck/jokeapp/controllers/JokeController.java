582370798cba08e69a66e6ba89e15c6b618a7311
hasanyesil <hasannyesil97@gmail.com>
2020-12-20 02:43:29 +0300

    Joke controller added.

---
+++ src/main/java/com/greenluck/jokeapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.greenluck.jokeapp.controllers;
+
+import com.greenluck.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke",jokeService.getJoke());
+        return "chucknorris";
+    }
+}
