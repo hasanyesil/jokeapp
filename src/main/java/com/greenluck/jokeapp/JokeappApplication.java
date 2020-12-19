021ff8da1a5b5d8f823aa3c1477cb85948ef07b9
hasanyesil <hasannyesil97@gmail.com>
2020-12-20 02:27:27 +0300

    Initial commit

---
+++ src/main/java/com/greenluck/jokeapp/JokeappApplication.java
@@ -0,0 +1,13 @@
+package com.greenluck.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+    }
+
+}
